10159c9f9e0ad1f3ec38b009859fd2cb87d0f4a9
ms-gda <support@gda.sa>
2023-08-04 20:44:15 +0300

    update [id] page

--- app/works/projects/[id]/page.jsx
+++ app/works/projects/[id]/page.jsx
@@ -5,10 +5,6 @@ const ProjectPage = ({ params }) => {
     <div>
       <h1 className="text-red-600">Project Page</h1>
       <h1 className="text-red-600">{params.id}</h1>
-      <img src={params.logo} alt={params.title} />
-      <p>{params.date}</p>
-      <h2>{params.clientName}</h2>
-      <h3>{params.scope}</h3>
     </div>
   );
 };
